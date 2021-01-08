d357cae0e2091283bffb658c80ef8961f7a55f02
BluBloos <cnoah1705@gmail.com>
2021-01-08 06:06:57 -0500

    Mover app.py to code

--- build.bat
+++ build.bat
@@ -1,5 +1,13 @@
 @echo off
+
+del bin\app.py
+
+pushd code
+copy app.py ..\bin\
+popd
+
 pushd bin
 
 cl -Zi -FeCollage -I ../lib ../code/Collage.cpp /link /LIBPATH:../lib -incremental:no user32.lib gdi32.lib Comdlg32.lib
+
 popd
